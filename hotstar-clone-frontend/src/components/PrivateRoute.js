ec5dba7076e586a85de86e7a822177bff149adbb
Kirtan-lokadiya <kirtanlokadiya998@gmail.com>
2024-02-07 18:22:08 +0530

    first commit

--- hotstar-clone-frontend/src/components/PrivateRoute.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Navigate, Outlet } from "react-router";
-
-const PrivateRoute = () => {
-  const auth = localStorage.getItem("token");
-  return auth ? <Outlet /> : <Navigate to="/signup" />;
-};
-
-export default PrivateRoute;
